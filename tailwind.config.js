e5ce47ae978645717e5e2374afe32edbfeb8110c
Bjarke Rønnow <bro@homerunner.com>
2025-01-14 23:13:21 +0100

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.html"],
+  theme: {
+    container: {
+      center: true,
+      padding: "2rem",
+    },
+    fontFamily: {
+      jetbrains: ["JetBrains Mono", "monospace"],
+    },
+    extend: {},
+  },
+  plugins: [],
+};
